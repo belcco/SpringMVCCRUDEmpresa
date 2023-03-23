2baad4fd1c3f0cd2d27ad4ff79a038825670e7bb
belco <bcollado91@hotmail.com>
2023-03-23 14:36:27 +0100

    timbreeee  paqueteee

--- src/main/java/com/example/SpringMvccrudEmpresaApplication.java
+++ src/main/java/com/example/SpringMvccrudEmpresaApplication.java
@@ -1,13 +1,23 @@
 package com.example;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
-public class SpringMvccrudEmpresaApplication {
+public class SpringMvccrudEmpresaApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringMvccrudEmpresaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		/**
+		 * 	REGISTROS
+		 */
 	}
 
 }
